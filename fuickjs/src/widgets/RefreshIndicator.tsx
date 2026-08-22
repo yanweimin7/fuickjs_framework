ca3f164654912f242ca0bada9f74648d9638e5d7
yanweimin7 <Yan7@Droid>
2026-08-23 02:06:33 +0800

    feat(framework): RefreshIndicator 提供 complete/show 命令式 API
    - 继承 BaseWidget，自动生成 refId，业务侧无需手动设置
    - complete()/show() 通过 callNativeCommand 派发到 Flutter 端

--- fuickjs/src/widgets/RefreshIndicator.tsx
+++ fuickjs/src/widgets/RefreshIndicator.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import { WidgetProps } from './types';
+import { BaseWidget } from './BaseWidget';
 
 export interface RefreshIndicatorProps extends WidgetProps {
   onRefresh?: () => void;
@@ -8,9 +9,20 @@ export interface RefreshIndicatorProps extends WidgetProps {
   refId?: string;
 }
 
-export class RefreshIndicator extends React.Component<RefreshIndicatorProps> {
+export class RefreshIndicator extends BaseWidget<RefreshIndicatorProps> {
   render(): ReactNode {
-    return React.createElement('RefreshIndicator', { ...this.props });
+    return React.createElement('RefreshIndicator', {
+      ...this.props,
+      refId: this.scopedRefId,
+    });
+  }
+
+  complete(): void {
+    this.callNativeCommand('complete');
+  }
+
+  show(): void {
+    this.callNativeCommand('show');
   }
 }
 
